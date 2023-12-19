498c550f6b02bd60952c17502a38d4089a3a298a
yoni <yoni@gmail.com>
2023-12-19 14:09:37 +0900

    add comment in storyline_crawling_code

--- storyline_crawling_code.py
+++ storyline_crawling_code.py
@@ -1,74 +1,53 @@
-import openpyxl
+#필요 라이브러리 임포트
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from openpyxl import load_workbook
 import time
 
-browser = webdriver.Chrome()
+# Chrome 웹드라이버 설정
+browser = webdriver.Chrome("./chromedriver")
 
-# -----------------------------------------------------------#
-# 드라마 제목 리스트
-from openpyxl import load_workbook
-
-read_xlsx = load_workbook('capstone_drama_01.xlsx')
+# 엑셀 파일 로드 및 드라마 이름 추출
+drama_data = 'C:\\Users\\82104\\PycharmProjects\\pythonProject\\capstone_drama_01.xlsx'
+read_xlsx = load_workbook(drama_data)
 read_sheet = read_xlsx.active
 name_col = read_sheet['D3:D1131']
 
-drama_names = []
-for cell_1 in name_col :
-    for cell_2 in cell_1 :
-        drama_names.append(cell_2.value)
+# 드라마 이름을 리스트에 저장
+drama_names = [cell_2.value for cell_1 in name_col for cell_2 in cell_1]
 
-# -----------------------------------------------------------#
-# 네이버에 드라마 검색 -> 리스트에 저장
-drama_storyline_list = []
+# 네이버 드라마 검색 및 에피소드 크롤링
+drama_episode_list = []
 
-for y in drama_names:
-    drama_search = y
+crawling_size = 10  # 10개씩 크롤링
+for i in range(0, len(drama_names), crawling_size):
+    batch_drama_names = drama_names[i: i + crawling_size]
 
-    browser.get(f"https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%ED%95%9C%EA%B5%AD%EB%93%9C%EB%9D%BC%EB%A7%88+{drama_search}")
+    for drama_search in batch_drama_names:
+        browser.get(f"https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%ED%95%9C%EA%B5%AD%EB%93%9C%EB%9D%BC%EB%A7%88+{drama_search}")
 
-    # Wait for the "펼쳐보기" button to be clickable
-    try:
-        element = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "story_more"))
-        )
-        # Click the "펼쳐보기" button
-        element.click()
+        html = browser.page_source
+        soup = BeautifulSoup(html, 'html.parser')
 
-        # Wait for the expanded content to be visible
-        WebDriverWait(browser, 10).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, "desc._text"))
-        )
-    except:
-        print("Error clicking the '펼쳐보기' button")
+        # 드라마 에피소드 정보 추출
+        drama_episode_1 = soup.select_one('div.detail_info > dl > div:nth-child(1) > dd > span > em')
 
-    html = browser.page_source
-    soup = BeautifulSoup(html, 'html.parser')
+        # 에피소드 정보 전처리 및 리스트에 추가
+        try:
+            drama_episode_2 = int(drama_episode_1.text.strip('부작'))
+            drama_episode_list.append(drama_episode_2)
+        except:
+            drama_episode_2 = 'fail'
+            drama_episode_list.append(drama_episode_2)
 
-    # Select the expanded storyline content
-    drama_episode_1 = soup.select_one('div.text_expand._img_ellipsis span.desc._text')
+        time.sleep(3)  # 각 요청 간의 3초 딜레이
 
-    try:
-        drama_episode_2 = drama_episode_1.text
-        drama_storyline_list.append(drama_episode_2)
-    except:
-        drama_episode_2 = 'fail'
-        drama_storyline_list.append(drama_episode_2)
-
-    time.sleep(1)
-
-# -----------------------------------------------------------#
-##drama_episode_list에 있는 줄거리를 기존엑셀에 저장
-
-#기존 엑셀의 F열에 3행부터 기입
+# 업데이트된 에피소드 정보로 엑셀 파일 업데이트
 column_to_update = 'F'
 start_row = 3
 
-for index, value in enumerate(drama_storyline_list) :
+for index, value in enumerate(drama_episode_list):
     read_sheet[f'{column_to_update}{start_row + index}'] = value
 
-#기존의 엑셀 파일에 저장
-read_xlsx.save('capstone_drama_01.xlsx')
+# 업데이트된 엑셀 파일 저장
+read_xlsx.save(drama_data)
