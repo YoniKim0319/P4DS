1f4446c398afadf37977c93b4901caac243f3b8a
yoni <yoni@gmail.com>
2023-12-19 14:47:50 +0900

    change storyline_crawling_code to final code

--- storyline_crawling_code.py
+++ storyline_crawling_code.py
@@ -1,53 +1,83 @@
-#필요 라이브러리 임포트
+import openpyxl
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from openpyxl import load_workbook
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# Chrome 웹드라이버 설정
-browser = webdriver.Chrome("./chromedriver")
+# 웹 드라이버 초기화
+browser = webdriver.Chrome()
 
-# 엑셀 파일 로드 및 드라마 이름 추출
-drama_data = 'C:\\Users\\82104\\PycharmProjects\\pythonProject\\capstone_drama_01.xlsx'
-read_xlsx = load_workbook(drama_data)
+# -----------------------------------------------------------#
+# 드라마 제목 리스트
+
+# 엑셀 파일 불러오기
+read_xlsx = load_workbook('capstone_drama_01.xlsx')
 read_sheet = read_xlsx.active
+
+# 엑셀에서 드라마 제목 데이터 추출
 name_col = read_sheet['D3:D1131']
+drama_names = []
+for cell_1 in name_col:
+    for cell_2 in cell_1:
+        drama_names.append(cell_2.value)
 
-# 드라마 이름을 리스트에 저장
-drama_names = [cell_2.value for cell_1 in name_col for cell_2 in cell_1]
+# 검증 코드: 처음 50개 항목만 가져오기
+# drama_names = [cell_2.value for cell_1 in name_col for cell_2 in cell_1][:50]
 
-# 네이버 드라마 검색 및 에피소드 크롤링
-drama_episode_list = []
+# -----------------------------------------------------------#
+# 네이버에 드라마 검색 -> 리스트에 저장
+drama_storyline_list = []
 
-crawling_size = 10  # 10개씩 크롤링
-for i in range(0, len(drama_names), crawling_size):
-    batch_drama_names = drama_names[i: i + crawling_size]
+for drama_name in drama_names:
+    drama_search = drama_name
 
-    for drama_search in batch_drama_names:
-        browser.get(f"https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%ED%95%9C%EA%B5%AD%EB%93%9C%EB%9D%BC%EB%A7%88+{drama_search}")
+    # 네이버 검색 페이지로 이동
+    browser.get(f"https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%ED%95%9C%EA%B5%AD%EB%93%9C%EB%9D%BC%EB%A7%88+{drama_search}")
 
-        html = browser.page_source
-        soup = BeautifulSoup(html, 'html.parser')
+    # "펼쳐보기" 버튼이 클릭 가능할 때까지 대기
+    try:
+        element = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "story_more"))
+        )
+        # "펼쳐보기" 버튼 클릭
+        element.click()
 
-        # 드라마 에피소드 정보 추출
-        drama_episode_1 = soup.select_one('div.detail_info > dl > div:nth-child(1) > dd > span > em')
+        # 확장된 내용이 보이도록 대기
+        WebDriverWait(browser, 10).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "desc._text"))
+        )
+    except:
+        print("Error clicking the '펼쳐보기' button")
 
-        # 에피소드 정보 전처리 및 리스트에 추가
-        try:
-            drama_episode_2 = int(drama_episode_1.text.strip('부작'))
-            drama_episode_list.append(drama_episode_2)
-        except:
-            drama_episode_2 = 'fail'
-            drama_episode_list.append(drama_episode_2)
+    # 현재 페이지 HTML을 가져와 BeautifulSoup으로 파싱
+    html = browser.page_source
+    soup = BeautifulSoup(html, 'html.parser')
 
-        time.sleep(3)  # 각 요청 간의 3초 딜레이
+    # 확장된 줄거리 내용 선택
+    drama_episode_1 = soup.select_one('div.text_expand._img_ellipsis span.desc._text')
 
-# 업데이트된 에피소드 정보로 엑셀 파일 업데이트
-column_to_update = 'F'
-start_row = 3
+    try:
+        drama_episode_2 = drama_episode_1.text
+        drama_storyline_list.append(drama_episode_2)
+    except:
+        drama_episode_2 = 'fail'
+        drama_storyline_list.append(drama_episode_2)
 
-for index, value in enumerate(drama_episode_list):
+    # 페이지 로딩 대기를 위한 sleep
+    time.sleep(1)
+
+# -----------------------------------------------------------#
+# drama_episode_list에 있는 줄거리를 기존 엑셀에 저장
+
+# 기존 엑셀의 F열에 3행부터 기입
+column_to_update = 'H'
+start_row = 2
+
+# 엑셀에 줄거리 추가
+for index, value in enumerate(drama_storyline_list):
     read_sheet[f'{column_to_update}{start_row + index}'] = value
 
-# 업데이트된 엑셀 파일 저장
-read_xlsx.save(drama_data)
+# 변경된 내용을 엑셀 파일에 저장
+read_xlsx.save('capstone_drama_01.xlsx')
