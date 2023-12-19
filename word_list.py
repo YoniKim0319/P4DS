7289a3249305b3495c2961cfec0c01d728f50cf9
AHN KYOYOUNG <ann120440@gmail.com>
2023-12-19 17:31:16 +0900

    딕셔너리 processing 완료

--- word_list.py
+++ word_list.py
@@ -1,65 +1,49 @@
-from openpyxl import load_workbook
-from konlpy.tag import Okt
-from matplotlib import font_manager as fm
-
-# 엑셀 파일에서 데이터 읽어오기
-wordcloud_data = 'storyline_update2.xlsx'
-read_xlsx = load_workbook(wordcloud_data, data_only=True)
-read_sheet = read_xlsx.active
-name_col = read_sheet['A1:A1128']
-
-# 줄거리를 행 별로 리스트로 만들기
-storyline = [cell.value for row in read_sheet.iter_rows(min_row=2, max_row=1128, min_col=1, max_col=1) for cell in row]
-
-# 한글 폰트 설정
-font_path = 'C:\\Users\\USER\\AppData\\Local\\Microsoft\\Windows\\Fonts\\NanumGothicBold.ttf'
-font_prop = fm.FontProperties(fname=font_path).get_name()
-
-# Matplotlib에서 한글 폰트 설정
-import matplotlib
-matplotlib.rc('font', family=font_prop)
-matplotlib.rcParams['axes.unicode_minus'] = False
-
-# Okt tokenizer 불러오기
-okt = Okt()
-
-# 리스트를 저장할 변수 초기화
-text_for_wordcloud_list = []
-
-# 각 행의 줄거리를 사용하여 WordCloud 생성 및 시각화
-for row_num, row in enumerate(read_sheet.iter_rows(min_row=2, max_row=1128, min_col=1, max_col=1), start=2):
-    storyline = row[0].value
-
-    # 텍스트 데이터를 전처리
-    text_data = ' '.join(storyline)
-
-    # Okt를 사용하여 명사와 형용사를 추출하기
-    tokens = okt.pos(text_data, stem=True)
-    filtered_tokens = [word for word, pos in tokens if pos in ['Noun', 'Adjective']]
-
-    # WordCloud에 사용할 텍스트로 필터링된 토큰을 결합
-    text_for_wordcloud = ' '.join(filtered_tokens)
-
-    # 리스트에 추가
-    text_for_wordcloud_list.append(text_for_wordcloud)
-
-print(text_for_wordcloud_list)
-
 import openpyxl
+from konlpy.tag import Komoran
 
-# 엑셀 파일 생성 또는 열기
-excel_file_path = 'drama_complete.xlsx'
+# 엑셀 파일 경로 지정
+excel_file_path = 'storyline_update2.xlsx'  # 실제 파일 경로로 변경해야 합니다.
+
+# 엑셀 파일 열기
 workbook = openpyxl.load_workbook(excel_file_path)
 
-# 원하는 시트 선택 (예: Sheet1)
-sheet = workbook['Sheet1']
+# 원하는 시트 선택 (예: 첫 번째 시트 선택)
+sheet = workbook.active
 
-# 파이썬 리스트 데이터 : 
+# KoNLPy의 Komoran 객체 생성
+komoran = Komoran()
 
-# 데이터를 G열의 2행부터 순차적으로 쓰기
-column_G = sheet['G']
-for i, value in enumerate(text_for_wordcloud_list, start=2):
-    column_G[i - 1].value = value
+# 명사와 형용사 추출
+nouns_and_adjectives_per_row_dict = {}
+for row in range(1, sheet.max_row + 1):
+    text = sheet.cell(row=row, column=1).value
+    if text:
+        pos_tags = komoran.pos(text)
+        extracted_words = [word for word, pos in pos_tags if pos in ['NN', 'NNG', 'NNP', 'VA', 'VAX', 'XR']]
+        nouns_and_adjectives_per_row_dict[row] = extracted_words
 
-# 엑셀 파일 저장
-workbook.save(excel_file_path)
+# 결과 확인 (딕셔너리)
+# for row, words in nouns_and_adjectives_per_row_dict.items():
+#     print(f"Row {row}: {words}")
+
+print(nouns_and_adjectives_per_row_dict)
+
+
+
+
+# 저장할 엑셀 파일 경로 지정
+output_excel_file_path = 'drama_complete.xlsx'
+
+# 기존 엑셀 파일 열기
+output_workbook = openpyxl.load_workbook(output_excel_file_path)
+
+# 원하는 시트 선택 (예: 첫 번째 시트 선택)
+output_sheet = output_workbook.active
+
+# 결과를 엑셀 파일에 저장 (G열에 저장)
+for row, words in nouns_and_adjectives_per_row_dict.items():
+    for col, word in enumerate(words, start=1):
+        output_sheet.cell(row=row, column=6 + col, value=word)
+
+# 저장한 내용을 엑셀 파일에 반영
+output_workbook.save(output_excel_file_path)
