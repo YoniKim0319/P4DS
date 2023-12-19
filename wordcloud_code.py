4e6b9481c03396b3272352c30a244ab7cc9f88a1
AHN KYOYOUNG <ann120440@gmail.com>
2023-12-19 15:51:02 +0900

    Merge wordcloud branch

--- wordcloud_code.py
+++ wordcloud_code.py
@@ -1,7 +1,7 @@
 from openpyxl import load_workbook
 from konlpy.tag import Okt
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
+# from wordcloud import WordCloud
+# import matplotlib.pyplot as plt
 from matplotlib import font_manager as fm
 
 # 엑셀 파일에서 데이터 읽어오기
@@ -16,14 +16,17 @@ storyline = [cell.value for row in read_sheet.iter_rows(min_row=2, max_row=1128,
 # 한글 폰트 설정
 font_path = 'C:\\Users\\USER\\AppData\\Local\\Microsoft\\Windows\\Fonts\\NanumGothicBold.ttf'
 font_prop = fm.FontProperties(fname=font_path).get_name()
+
+# Matplotlib에서 한글 폰트 설정
 import matplotlib
 matplotlib.rc('font', family=font_prop)
-
-# 유니코드 깨짐현상 해결
 matplotlib.rcParams['axes.unicode_minus'] = False
 
 # Okt tokenizer 불러오기
 okt = Okt()
+
+# 리스트를 저장할 변수 초기화
+text_for_wordcloud_list = []
 
 # 각 행의 줄거리를 사용하여 WordCloud 생성 및 시각화
 for row_num, row in enumerate(read_sheet.iter_rows(min_row=2, max_row=1128, min_col=1, max_col=1), start=2):
@@ -39,6 +42,9 @@ for row_num, row in enumerate(read_sheet.iter_rows(min_row=2, max_row=1128, min_
     # WordCloud에 사용할 텍스트로 필터링된 토큰을 결합
     text_for_wordcloud = ' '.join(filtered_tokens)
 
+    # 리스트에 추가
+    text_for_wordcloud_list.append(text_for_wordcloud)
+'''
     # wordcloud 생성
     wordcloud = WordCloud(
         font_path=font_path,
@@ -47,22 +53,34 @@ for row_num, row in enumerate(read_sheet.iter_rows(min_row=2, max_row=1128, min_
         height=400,
         max_words=100,  # 필요에 따라 조절
     ).generate(text_for_wordcloud)
+'''
+print(text_for_wordcloud_list)
 
+'''
+import openpyxl
+
+# 엑셀 파일 생성 또는 열기
+excel_file_path = 'drama_complete.xlsx'
+workbook = openpyxl.load_workbook(excel_file_path)
+
+# 원하는 시트 선택 (예: Sheet1)
+sheet = workbook['Sheet1']
+
+# 파이썬 리스트 데이터 : 
+
+# 데이터를 G열의 2행부터 순차적으로 쓰기
+column_G = sheet['G']
+for i, value in enumerate(data_list, start=2):
+    column_G[i - 1].value = value
+
+# 엑셀 파일 저장
+workbook.save(excel_file_path)
+'''
+'''
     # 시각화
     plt.figure(figsize=(10, 5))
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')
     plt.title(f'WordCloud for Row {row_num}')
     plt.show()
-
-
-#-----------------------------------------------------------#
-# 기존 엑셀의 H열에 2행부터 기입
-column_to_update = 'H'
-start_row = 2
-
-for index, value in enumerate(text_for_wordcloud) :
-    read_sheet[f'{column_to_update}{start_row + index}'] = value
-
-#기존의 엑셀 파일에 저장
-read_xlsx.save(capstone_drama_no_fail)
+'''
